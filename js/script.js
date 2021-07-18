d5e14452fee42203f113d2fde3df58a6df00604c
전준휘 <jjh63360@naver.com>
2021-07-18 17:41:31 +0900

    Add IntoTheStars Background

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
-let scrollTop = 0;
+let oldScrollTop=0,newScrollTop = 0, scrollPercent=0;
 let imageAll;
 let totalNum = 0;
-let moveMouse, stars1, stars2, stars3, text3d, IntoTheStarsPage;
+let moveMouse, stars1, stars2, stars3, text3d, intoTheStars;
 let x = 0;
 let y = 0;
 let mx = 0;
 let my = 0;
 let speed = 0.009;
+let scrollCnt = 0;
+
+var context, canvas;
 
 window.onload = function(){
   stars1 = document.getElementById("stars1");
@@ -14,12 +17,12 @@ window.onload = function(){
   stars3 = document.getElementById("stars3");
   moveMouse = document.getElementById("moveMouse");
   text3d = document.getElementsByClassName("text3d")[0];
-  IntoTheStarsPage = document.getElementsByClassName("starPage")[0];
+  intoTheStars = document.getElementById("intoTheStars");
 
   window.addEventListener("mousemove", mouseFunc, false);
 
   // window.addEventListener('resize', stageResize, false);
-  //window.addEventListener('scroll', scrollFunc);
+  window.addEventListener('scroll', scrollFunc);
   function mouseFunc(e)
   {
     x = (e.clientX - window.innerWidth / 2);
@@ -47,23 +50,33 @@ function loop(){
 
 
 
-// function scrollFunc(e) {
-//     scrollTop = this.scrollY;
+function scrollFunc(e) {
+    newScrollTop = document.documentElement.scrollTop;
+    scrollPercent = Math.ceil((newScrollTop/document.body.scrollHeight)*100);
 
-//     // let per = Math.ceil(scrollTop / (_documentHum - _windowHNum) * 100);
-//     // progressBar.style.width = per + "%";
+    intoTheStars.style.opacity = (newScrollTop/(document.body.scrollHeight)*20);
+    console.log( intoTheStars.style.opacity);
 
-//     for(var i=0; i< totalNum ; i++){
-//         imageAll[i].style.transform = "perspective(400px) translateZ("+ scrollTop/(5*(totalNum-i)) +"px)";
-//         // imageAll[i].style.transform = "perspective(400px) translateZ("+ scrollTop/5 +"px)";
-//         console.log(scrollTop , scrollTop / (5*(totalNum-i)) );
-//     }
+    if(scrollPercent<=0.01) intoTheStars.style.opacity = 0;
+     
+      if(newScrollTop-oldScrollTop>20)
+      {
+        scrollCnt += 1;
+        console.log(scrollCnt%40);
+        drawIntoTheStars(scrollCnt%40);
+      }
+      else if(newScrollTop-oldScrollTop<-20 && scrollCnt>0)
+      {
+        scrollCnt -= 1;
+        console.log(scrollCnt%40);
+        drawIntoTheStars(scrollCnt%40);
+      }
+    
+    oldScrollTop = newScrollTop;
+}
 
-// }
-
-// function stageResize() {
-//     _documentHum = document.body.offsetHeight; 
-//     _windowHNum = window.outerHeight; 
-// }
-
+function  drawIntoTheStars(i)
+{
+  document.getElementById('intoTheStars').src = "./image/Space Travel/IntoTheStars"+i+".jpg";
+}
 
